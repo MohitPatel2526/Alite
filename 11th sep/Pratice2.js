c7410b32c71c72df13d42e01265408b28bf45aa0
Your Name <you@example.com>
2025-09-12 14:57:40 +0530

    12/09/2025

---
+++ 11th sep/Pratice2.js
@@ -0,0 +1,12 @@
+//JAVA SCRIPT IN <hea> or <body>
+
+//you can place any number of scripts in an HTML docoment.
+//Scripts can be placed in the  <body>, or in the <head> section of an HTML page,or in both.
+
+//  JAVASCRIPT IN <HEAD>
+// in this example, a js function is placed in the <head> section of an HTML page.
+ // The f.n. is invoked (called) when a biutton is clicked.
+
+function myfunction() {
+    decodeURIComponent.getElementById("demo"). innerHTML = "paraggraph changed.";
+}
